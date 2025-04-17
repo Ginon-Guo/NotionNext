43e45a4984258eb036cf3b2a0c10807e7be15fb4
Ginon-Guo <guohaobin2000@gmail.com>
2025-04-17 17:11:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/Ginon-Guo',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://ginon-notion.vercel.app',
+  HEO_INFO_CARD_URL3: 'https://ginon.vercel.app',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -118,9 +118,9 @@ const CONFIG = {
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_2: '加入社群讨论分享(暂未开放)',
+  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群(暂未开放)',
+  HEO_SOCIAL_CARD_URL: '',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
